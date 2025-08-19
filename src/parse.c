93556372b178581955a8eca091ff3b9fa9649cc1
Abdulaziz Alrashidi <aziz_alreshidy@hotmail.com>
2025-08-19 01:13:36 -0400

    first

--- src/parse.c
+++ src/parse.c
@@ -10,28 +10,28 @@
 #include "common.h"
 #include "parse.h"
 
-void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees) {
-    dbhdr = NULL;
-    employees = NULL;
-}
+// void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees) {
+//     dbhdr = NULL;
+//     employees = NULL;
+// }
 
-int add_employee(struct dbheader_t *dbhdr, struct employee_t *employees, char *addstring) {
-    dbhdr = NULL;
-    employees = NULL;
-    addstring = NULL;
-}
+// int add_employee(struct dbheader_t *dbhdr, struct employee_t *employees, char *addstring) {
+//     dbhdr = NULL;
+//     employees = NULL;
+//     addstring = NULL;
+// }
 
-int read_employees(int fd, struct dbheader_t *dbhdr, struct employee_t **employeesOut) {
-    return 0;
-}
+// int read_employees(int fd, struct dbheader_t *dbhdr, struct employee_t **employeesOut) {
+//     return 0;
+// }
 
-int output_file(int fd, struct dbheader_t *dbhdr, struct employee_t *employees) {
+// int output_file(int fd, struct dbheader_t *dbhdr, struct employee_t *employees) {
 
-}	
+// }	
 
-int validate_db_header(int fd, struct dbheader_t **headerOut) {
+// int validate_db_header(int fd, struct dbheader_t **headerOut) {
 
-}
+// }
 
 int create_db_header(int fd, struct dbheader_t **headerOut) {
     struct dbheader_t* header = calloc(1, sizeof(struct dbheader_t));
